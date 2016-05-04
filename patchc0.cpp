7fe72c6d29a5fbbcc66374736ca8a2f578157b32
tamk <>
2016-05-04 22:49:21 +0200

    Other ribs. Fixed rib indexing. Fixed last rib.

--- patchc0.cpp
+++ patchc0.cpp
@@ -65,8 +65,8 @@ PatchC0::PatchC0(glm::vec4 center,
             segments.append(bezier);
         }
     }
-    for(int i=0;i<(patches_u);++i){
-        for(int j=0;j<patches_v;++j){
+    for(int i=0;i<patches_u;++i){
+        for(int j=0;j<patches_v+1;++j){
             Bezier3* bezier = new Bezier3({control[3*i*points_v+3*j],
                                           control[(3*i+1)*points_v+3*j],
                                           control[(3*i+2)*points_v+3*j],
@@ -122,7 +122,7 @@ void PatchC0::updateRibsChanged(int udiv, int vdiv)
         man->removeDrawable(segments[i]);
         //internal ribs own their points
         for(int j=0;j<4;++j){
-            PointCAM*p = segments[i]->children()[i];
+            PointCAM*p = segments[i]->children()[j];
             man->removePoint(p);
             delete p;
         }
@@ -138,14 +138,14 @@ void PatchC0::updateRibsChanged(int udiv, int vdiv)
                 float t = 1-u;
                 float b0=t*t*t,b1=3*t*t*u,b2=3*t*u*u,b3=u*u*u;
                 for(int l=0;l<4;++l){
-                    ps[i]=new PointCAM(glm::vec3{
+                    ps[l]=new PointCAM(glm::vec3{
                                            control[3*i*points_v+3*j+l]->pos()*b0+
                                            control[(3*i+1)*points_v+3*j+l]->pos()*b1+
                                            control[(3*i+2)*points_v+3*j+l]->pos()*b2+
                                            control[(3*i+3)*points_v+3*j+l]->pos()*b3
                                        },man);
-                    man->addPoint(ps[i]);
-                    auto p =ps[i];
+                    man->addPoint(ps[l]);
+                    auto p =ps[l];
                     auto update=[this,b0,b1,b2,b3,p,i,j,l](PointCAM*){
                         p->setPos(glm::vec3{
                                       control[3*i*points_v+3*j+l]->pos()*b0+
@@ -163,57 +163,45 @@ void PatchC0::updateRibsChanged(int udiv, int vdiv)
                     connect(control[(3*i+3)*points_v+3*j+l],&PointCAM::pointChanged,
                             update);
                 }
-                /*PointCAM*p0=new PointCAM(glm::vec3{
-                                             control[3*i*points_v+3*j]->pos()*(1-u)+
-                                             control[(3*i+3)*points_v+3*j]->pos()*u
-                                         },man);
-                PointCAM*p1=new PointCAM(glm::vec3{
-                                             control[3*i*points_v+3*j+1]->pos()*(1-u)+
-                                             control[(3*i+3)*points_v+3*j+1]->pos()*u
-                                         },man);
-                PointCAM*p2=new PointCAM(glm::vec3{
-                                             control[3*i*points_v+3*j+2]->pos()*(1-u)+
-                                             control[(3*i+3)*points_v+3*j+2]->pos()*u
-                                         },man);
-                PointCAM*p3=new PointCAM(glm::vec3{
-                                             control[3*i*points_v+3*j+3]->pos()*(1-u)+
-                                             control[(3*i+3)*points_v+3*j+3]->pos()*u
-                                         },man);
-                man->addPoint(p0);
-                man->addPoint(p1);
-                man->addPoint(p2);
-                man->addPoint(p3);
-                Bezier3*bezier = new Bezier3({p0,p1,p2,p3});*/
                 Bezier3*bezier = new Bezier3({ps[0],ps[1],ps[2],ps[3]});
                 man->addDrawable(bezier);
                 segments.append(bezier);
             }
-            /*for(int k=0;k<vdiv;++k){
-                float u=(k+1.0)/(vdiv+1.0);
-                PointCAM*p0=new PointCAM(glm::vec3{
-                                             control[(3*i)*points_v+3*j]->pos()*(1-u)+
-                                             control[(3*i)*points_v+3*j+3]->pos()*u
-                                         },man);
-                PointCAM*p1=new PointCAM(glm::vec3{
-                                             control[(3*i+1)*points_v+3*j]->pos()*(1-u)+
-                                             control[(3*i+1)*points_v+3*j+3]->pos()*u
-                                         },man);
-                PointCAM*p2=new PointCAM(glm::vec3{
-                                             control[(3*i+2)*points_v+3*j]->pos()*(1-u)+
-                                             control[(3*i+2)*points_v+3*j+3]->pos()*u
-                                         },man);
-                PointCAM*p3=new PointCAM(glm::vec3{
-                                             control[(3*i+3)*points_v+3*j]->pos()*(1-u)+
-                                             control[(3*i+3)*points_v+3*j+3]->pos()*u
-                                         },man);
-                man->addPoint(p0);
-                man->addPoint(p1);
-                man->addPoint(p2);
-                man->addPoint(p3);
-                Bezier3*bezier = new Bezier3({p0,p1,p2,p3});
+            for(int k=0;k<vdiv;++k){
+                float v=(k+1.0)/(vdiv+1.0);
+                PointCAM* ps[4] = {nullptr,nullptr,nullptr,nullptr,};
+                float t = 1-v;
+                float b0=t*t*t,b1=3*t*t*v,b2=3*t*v*v,b3=v*v*v;
+                for(int l=0;l<4;++l){
+                    ps[l]=new PointCAM(glm::vec3{
+                                           control[(3*i+l)*points_v+3*j]->pos()*b0+
+                                           control[(3*i+l)*points_v+3*j+1]->pos()*b1+
+                                           control[(3*i+l)*points_v+3*j+2]->pos()*b2+
+                                           control[(3*i+l)*points_v+3*j+3]->pos()*b3
+                                       },man);
+                    man->addPoint(ps[l]);
+                    auto p =ps[l];
+                    auto update=[this,b0,b1,b2,b3,p,i,j,l](PointCAM*){
+                        p->setPos(glm::vec3{
+                                      control[(3*i+l)*points_v+3*j]->pos()*b0+
+                                      control[(3*i+l)*points_v+3*j+1]->pos()*b1+
+                                      control[(3*i+l)*points_v+3*j+2]->pos()*b2+
+                                      control[(3*i+l)*points_v+3*j+3]->pos()*b3
+                                  });
+                    };
+                    connect(control[(3*i+l)*points_v+3*j],&PointCAM::pointChanged,
+                            update);
+                    connect(control[(3*i+l)*points_v+3*j+1],&PointCAM::pointChanged,
+                            update);
+                    connect(control[(3*i+l)*points_v+3*j+2],&PointCAM::pointChanged,
+                            update);
+                    connect(control[(3*i+l)*points_v+3*j+3],&PointCAM::pointChanged,
+                            update);
+                }
+                Bezier3*bezier = new Bezier3({ps[0],ps[1],ps[2],ps[3]});
                 man->addDrawable(bezier);
                 segments.append(bezier);
-            }*/
+            }
 
         }
     }
